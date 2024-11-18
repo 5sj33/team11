010e120d4bb465e8fe65d08078b9c0760069cdb3
5sj33 <20143@hakodate.kosen-ac.jp>
2024-11-18 13:51:42 +0900

    add charactor

--- game11/kate.java
+++ game11/kate.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class kame here.
+ * Write a description of class kate here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class kame extends Actor
+public class kate extends Actor
 {
     /**
-     * Act - do whatever the kame wants to do. This method is called whenever
+     * Act - do whatever the kate wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- game11/keropi.java
+++ game11/keropi.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class a here.
+ * Write a description of class keropi here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class a extends Actor
+public class keropi extends Actor
 {
     /**
-     * Act - do whatever the a wants to do. This method is called whenever
+     * Act - do whatever the keropi wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
