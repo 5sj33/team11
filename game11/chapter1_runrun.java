3d570849ac9cfd04380c15c172e9c13f9bb0e8ec
5sj33 <20143@hakodate.kosen-ac.jp>
2024-11-25 14:29:14 +0900

    add chapter1_sub

---
+++ game11/chapter1_runrun.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class chapter1_runrun here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class chapter1_runrun extends World
+{
+
+    /**
+     * Constructor for objects of class chapter1_runrun.
+     * 
+     */
+    public chapter1_runrun()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
