36cdcd75d735c73c56bd988b7d2b92814dcc6351
WatanabeMasato723 <18198@hakodate.kosen-ac.jp>
2024-11-25 13:35:23 +0900

    cが更新

--- game11/smalltower.java
+++ game11/smalltower.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class a here.
+ * Write a description of class smalltower here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class a extends Actor
+public class smalltower extends Actor
 {
     /**
-     * Act - do whatever the a wants to do. This method is called whenever
+     * Act - do whatever the smalltower wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
